8f03e48ffdfc3dbd0705d27fd0c04e7b293454a2
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-08-11 18:51:26 +1000

    wip 3 data_source cleaned

--- WebsiteCreator/data_sources.py
+++ WebsiteCreator/data_sources.py
@@ -1,70 +1,60 @@
-"""File contains the DataSource class"""
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 
 import os
 import pandas as pd
 
 
 class DataSource():
-    """Gets file directories and data source from underlying files (sometimes
-    requiring merging and filtering and returns data as pandas dataframes)
-    """
 
     @property
-    def website_creator_directory(self):
-        """Returns the directory containing various files utilised to
-        create the hugo website by relative reference to the directory
-        of the file containgin this code"""
+    def website_creator_directory(self) -> str:
+        """Returns the directory containing various code utilised to
+        generate content in order to create the hugo website"""
         this_file_path = os.path.dirname(__file__)
         return this_file_path
 
     @property
-    def docs_directory(self):
-        """Returns the docs directory used to generate Hugo website content.
-        The directory path is determined by relative reference the file
-        containing this code"""
+    def docs_directory(self) -> str:
+        """Returns the docs directory containinng content of Hugo website"""
         this_file_path = os.path.dirname(__file__)
         return (os.path.join(
             os.path.dirname(this_file_path),
             'content', 'docs'))
 
     @property
-    def hierarchies_file_path(self):
-        """Returns directory string of site_hierarchy.csv file"""
+    def hierarchies_file_path(self) -> str:
         return (self.website_creator_directory
                 + os.path.sep
                 + 'site_hierarchy.csv')
 
     @property
-    def formulas_file_path(self):
-        """Returns directory string of formulas.csv file"""
+    def formulas_file_path(self) -> str:
         return (self.website_creator_directory + os.path.sep
                 + 'formulas.csv')
 
     @property
-    def definitions_file_path(self):
-        """Returns directory string of definitions.csv file"""
+    def definitions_file_path(self) -> str:
         return (self.website_creator_directory + os.path.sep
                 + 'definitions.csv')
 
     @property
-    def syllabus_file_path(self):
-        """Returns directory string of syllabus_topics.csv file"""
+    def syllabus_file_path(self) -> str:
         return (self.website_creator_directory + os.path.sep
                 + 'syllabus_topics.csv')
 
     @property
-    def subject_dependencies_file_path(self):
-        """Returns directory string of subject_dependencies.csv file"""
+    def subject_dependencies_file_path(self) -> str:
         return (self.website_creator_directory + os.path.sep
                 + 'subject_dependencies.csv')
 
     @property
-    def syllabus_by_year(self):
-        """Returns the syllabus data as dataframe"""
+    def syllabus_by_year(self) -> pd.DataFrame:
         return pd.read_csv(self.syllabus_file_path)
 
     @property
-    def syllabus_by_year_cumulative(self):
+    def syllabus_by_year_cumulative(self) -> pd.DataFrame:
         """Returns syllabus details dataframe on a cumulative level by subject
         order  for a given state.  (includes the current subject syllabus
         as well as the syllabus from a subjects dependencies)
@@ -83,23 +73,18 @@ class DataSource():
         return return_value
 
     @property
-    def subject_dependencies(self):
-        """Returns the subject dependencies as a pandas dataframe
+    def subject_dependencies(self) -> pd.DataFrame:
+        """Returns the subject dependencies (subjects which have assumed
+        knowledge for given subject)
         """
         return pd.read_csv(self.subject_dependencies_file_path)
 
     @property
-    def site_hierarchies(self):
-        """Returns the site hierarchy data as a pandas dataframe
-        """
+    def site_hierarchies(self) -> pd.DataFrame:
         return pd.read_csv(self.hierarchies_file_path)
 
     @property
-    def formulas_by_year(self):
-        """Returns dataframe of formulas and related fields by merging
-        formulas (ex-syllabus) and syllabus files
-        """
-
+    def formulas_by_year(self) -> pd.DataFrame:
         # TODO Temporarily mark empty boolean fields as false to avoid type errors
         # Remove once all data capture is complete
         formulas_input_converter = {
@@ -115,8 +100,8 @@ class DataSource():
         return formulas
 
     @property
-    def formulas_by_year_cumulative(self):
-        """Returns formula details dataframe on a cumulative level by subject
+    def formulas_by_year_cumulative(self) -> pd.DataFrame:
+        """Returns formula details on a cumulative level by subject
         order  for a given state.  (includes the current subjects formulas
         as well as the formulas from a subjects dependencies)
         """
@@ -138,9 +123,7 @@ class DataSource():
         return return_value
 
     @property
-    def definitions_by_year(self):
-        """Returns the maths definitions as a pandas dataframe
-        """
+    def definitions_by_year(self) -> pd.DataFrame:
         definitions_data = pd.read_csv(self.definitions_file_path)
 
         definitions_data = pd.merge(
